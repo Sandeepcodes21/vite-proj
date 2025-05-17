a6020af1faa068a103ca2c9bc28dd0db4ab72aa4
Sandeepcodes21 <sandeepcode.21@gmail.com>
2025-05-17 10:02:26 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.xlsx']
+  assetsInclude: ['**/*.xlsx'],
+  server: {
+    proxy: {
+      '/generate': 'https://qgeni.onrender.com'
+    }
+  }
 });
